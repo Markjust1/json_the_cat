fb063f8ab109bc5cf506079bc32d5f8acbf0945d
Artem Novikov <novikov711@gmail.com>
2022-05-12 22:22:06 +0000

    Tested breedFetcher.js for existing and non-existing breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const codeId = args;
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (!data.length) {
+      return callback('Cat not found');
+    }
+    const cat = data[0];
+    const catDescription = cat.description;
+    //console.log(catDescription)
+    callback(null, catDescription);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${codeId}`, function(error, response, body) {
-  if (error) {
-    console.log('Request was unsuccessful');
-    return;
-  }
-  // console.log('statusCode:', response && response.statusCode);
-  // console.log('body:', body);
-  // console.log('typeof body:', typeof body)
-  const data = JSON.parse(body);
-  if (!data.length) {
-    return console.log('Cat not found');
-  }
-  const cat = data[0];
-  console.log(cat.description);
-});
+module.exports = {fetchBreedDescription};
